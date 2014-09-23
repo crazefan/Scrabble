6568b5b9288c625b5818b9092889b1a10c0bdbd3
Timur <timur_gt@mail.ru>
2014-09-23 21:03:19 +0600

    instructions added

---
+++ modular functions/input.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	cout << "Please enter a valid sentence (with spaces):\n>";
+    getline(cin, input);
+	return 0;
+}
